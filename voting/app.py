a2f67245478fea27d5c0c1c5448436c63b7c7f4c
Laura Angélica <lacardenasv@unal.edu.co>
2018-02-28 01:28:39 +0000

    :sparkles: changing the voting options

--- voting/app.py
+++ voting/app.py
@@ -9,8 +9,8 @@ import logging
 import signal
 import sys
 
-optionA = "Dev"
-optionB = "Ops"
+optionA = "Ballet"
+optionB = "Cine"
 name = optionA + " VS " + optionB
 
 redis = Redis(host="redis", db=0)
